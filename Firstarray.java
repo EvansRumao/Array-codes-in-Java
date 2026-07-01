ac9a6d58a37b8390b8ea9d146343d951832b3b15
EvansRumao <evansrumao30@gmail.com>
2026-07-01 18:15:10 +0530

    Add array input and search functionality

--- Firstarray.java
+++ Firstarray.java
@@ -24,3 +24,45 @@ public class Firstarray {
 
     }
 }
+
+import java.util.Scanner;
+
+public class Firstarray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Read the size of the array
+        System.out.print("Enter the length of the array: ");
+        int[] arr = new int[sc.nextInt()];
+
+        // Read array elements
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Read the element to search
+        System.out.print("Enter the element you want to find: ");
+        int target = sc.nextInt();
+
+        // Search for the element
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("Element found at position " + (i + 1));
+                found = true;
+                break; // Stop searching once found
+            }
+        }
+
+        // If element is not found
+        if (!found) {
+            System.out.println("Element not found.");
+        }
+
+        sc.close();
+    }
+}
